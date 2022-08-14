26f251ee0b11b5394374a30782c5c18b700e864c
Kira <starkial@lenta.ru>
2022-08-14 12:34:27 +0500

    Task 023 программа выдаёт кубы

---
+++ Task023/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count < N)
+{
+    int a = Convert.ToInt32(Math.Pow(count, 3));
+    Console.WriteLine(a);
+    count ++;
+}
