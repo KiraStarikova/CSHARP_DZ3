7da35d19fb767528061bd628865eed26bcd08242
Kira <starkial@lenta.ru>
2022-08-14 14:05:31 +0500

    Task 019 программа определения палиндрома

---
+++ Task019/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine( ));
+string str_num = num.ToString();
+char digit1 = str_num[0];
+char digit2 = str_num[1];
+char digit4 = str_num[3];
+char digit5 = str_num[4];
+if (digit1 == digit5 && digit2 == digit4)
+Console.WriteLine($"Число {num} является палиндромом");
+else
+Console.WriteLine($"Число {num} не является палиндромом");
+
