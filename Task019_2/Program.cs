b95f15f8ba3227289cb431c144fc62d6e8131507
Kira <starkial@lenta.ru>
2022-08-15 14:16:39 +0500

    Task 19 2 решение через функцию задача палиндром

---
+++ Task019_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine( );
+void Proverka (string num)
+{
+    if (num [0] == num [4] && num [1] == num [3])
+    {
+        Console.WriteLine($"Число {num} является палиндромом");
+    }
+    else
+        Console.WriteLine($"Число {num} не является палиндромом");
+}
+if  (num.Length == 5)
+{
+    Proverka (num);
+}
+else Console.WriteLine("Введите пятизначное число");
